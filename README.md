18e770d6c68ac47464eee909a4395dbd7fbca24e
Andy Park <atpark333@gmail.com>
2023-02-25 00:04:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# The Brick Breaker Game
+
+A simple [BreakOut](https://en.wikipedia.org/wiki/Breakout_(video_game)) clone.
+
+TODO: Add screenshot
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Create a frame
+
+        // TODO: Instantiate GamePlay
+
+        // TODO: Configure and display frame
+
+        // TODO: Add gameplay instance to frame
+    }
+}
